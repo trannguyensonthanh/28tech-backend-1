1815be43200db8bbcf8c7b4e9442ccd9557572b9
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-15 12:06:52 +0700

    1. Thêm authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,4 +1,5 @@
 const Account = require("../../models/account.model.js")
+const Role = require("../../models/role.model.js")
 const systemConfig = require("../../config/system.js");
 module.exports.requireAuth = async (req, res, next) => {
   
@@ -6,11 +7,16 @@ module.exports.requireAuth = async (req, res, next) => {
   res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
   } else {
     console.log(req.cookies.token);  // lấy cookies từ link req
-  const user = await Account.findOne({token: req.cookies.token});
+  const user = await Account.findOne({token: req.cookies.token}).select("-password");
   if (!user) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
   }
   else {
+   const role = await Role.findOne({
+    _id: user.role_id
+   }).select("title permissions")
+   res.locals.role = role;
+    res.locals.user = user;
    next();
   }
    
