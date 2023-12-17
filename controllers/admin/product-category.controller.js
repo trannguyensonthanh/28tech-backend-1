52381841d89dfde7135003cb32188658d2171da2
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-17 11:44:32 +0700

    fix phân quyền bên backend

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -39,16 +39,22 @@ const newRecords = createTreeHelper.tree(records);
 
 // [post] /admin/products-category/create
 module.exports.createPost = async (req, res) => {
-if (req.body.position == ""){
-  const count = await ProductCategory.countDocuments();
-  req.body.position = count + 1;
+// kiểm tra phân quyền bên backend
+const permissions = res.locals.role.permissions;
+if (permissions.includes("products-category_create")){
+  if (req.body.position == ""){
+    const count = await ProductCategory.countDocuments();
+    req.body.position = count + 1;
+  }
+  else {
+    req.body.position = parseInt(req.body.position);
+  }
+  
+  const record = new ProductCategory(req.body);
+  await record.save();
+} else {
+ return;
 }
-else {
-  req.body.position = parseInt(req.body.position);
-}
-
-const record = new ProductCategory(req.body);
-await record.save();
 res.redirect(`${systemConfig.prefixAdmin}/products-category`);
 }
 
