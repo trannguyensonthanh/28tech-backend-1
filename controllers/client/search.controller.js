b9b4dd2feddef6dd83b25638a7fdefa14c4da759
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-18 13:19:44 +0700

    làm trang kết quả tìm kiếm

---
+++ controllers/client/search.controller.js
@@ -0,0 +1,23 @@
+const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
+// [get] /search
+module.exports.index = async (req, res) => {
+ 
+const keyword = req.query.keyword;
+let newProducts = []
+if (keyword){
+  const keywordRegex = new RegExp(keyword, "i");
+  const products = await Product.find({
+    title: keywordRegex,
+    deleted: false,
+    status: "active"
+  })
+  newProducts = productsHelper.priceNewProducts(products);
+ console.log(newProducts)
+}
+res.render("client/pages/search/index.pug", {
+  pageTitle : "Kết quả tìm kiếm",
+  keyword: keyword,
+  newProducts: newProducts
+});
+}
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,10 +1,12 @@
 
 const productRoutes = require("../client/products.route")
 const homeRoutes = require("../client/home.route")
+const searchRoutes = require("../client/search.route")
 const categoryMiddleware = require("../../middlewares/client/category.middleware")
   module.exports = (app) => {
    app.use(categoryMiddleware.category)
     app.use('/', homeRoutes);
   app.use('/products', productRoutes);
+  app.use('/search', searchRoutes);
   
   }
