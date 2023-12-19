2282b99494ea0c3d396a44400196158bc6bfd618
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-19 15:42:17 +0700

    làm giao diện

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,6 +3,7 @@ const productRoutes = require("../client/products.route")
 const homeRoutes = require("../client/home.route")
 const searchRoutes = require("../client/search.route")
 const cartRoutes = require("../client/cart.route")
+const checkoutRoutes = require("../client/checkout.route")
 const categoryMiddleware = require("../../middlewares/client/category.middleware")
 const cartMiddleware = require("../../middlewares/client/cart.middleware")
   module.exports = (app) => {
@@ -12,5 +13,6 @@ const cartMiddleware = require("../../middlewares/client/cart.middleware")
   app.use('/products', productRoutes);
   app.use('/search', searchRoutes);
   app.use('/cart', cartRoutes);
+  app.use('/checkout', checkoutRoutes);
   
   }
