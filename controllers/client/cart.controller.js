755003937f383438319e11269801b4d84d8fd439
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-19 14:40:22 +0700

    xóa sản phẩm trong giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -75,3 +75,14 @@ cart.totalPrice = cart.products.reduce( (sum, item) => {
     cartDetail: cart
 });
 }
+
+// [get] /cart/delete/:productId
+module.exports.delete = async (req, res) => {
+  const cartId = req.cookies.cartId;
+const productId = req.params.productId;
+await Cart.updateOne({_id: cartId}, {
+  "$pull" :{ products : {"product_id" : productId}}
+});
+req.flash("success", " Đã xóa thành công");
+res.redirect("back");
+}
--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -5,5 +5,6 @@ const controller = require("../../controllers/client/cart.controller")
 
 router.post('/add/:productId', controller.addPost)
 router.get('/', controller.index)
+router.get('/delete/:productId', controller.delete)
 
 module.exports = router;
