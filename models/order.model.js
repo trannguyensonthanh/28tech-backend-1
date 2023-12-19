8eff151f0be5d7c38d4e1b1a0aa77064866add49
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-19 15:19:43 +0700

    tạo collection và model

---
+++ models/order.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const orderSchema = new mongoose.Schema({
+    
+cart_id: String,
+userInfo: {
+  fullName : String,
+  phone: String,
+  address: String
+},
+products: [
+  {
+    products_id: String,
+    price: Number,
+    discountPercentage: Number,
+    quantity: Number,
+  }
+]
+}, {
+  timestamps: true
+});
+
+const Order = mongoose.model("Order", orderSchema, "order"); //Role là tên của model này, roleSchema là tên của định dạng,  roles là tên trong mongodb
+
+module.exports = Order;  // chỉ export cái thằng Role 
