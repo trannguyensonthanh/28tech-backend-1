bfdafe9d341e9da814619198aa3d72e1b8d35101
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-21 14:43:49 +0700

    Trang dashboard bên admin

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,44 @@
-module.exports.dashboard = (req, res) => {
+const ProductCategory = require("../../models/product-category.model")
+
+
+module.exports.dashboard = async (req, res) => {
+
+   const statistic = {
+      categoryProduct: {
+         total: 0,
+         active: 0,
+         inactive: 0
+      },
+      product: {
+         total: 0,
+         active: 0,
+         inactive: 0
+      },
+      account: {
+         total: 0,
+         active: 0,
+         inactive: 0
+      },
+      user: {
+         total: 0,
+         active: 0,
+         inactive: 0
+      },
+   }
+statistic.categoryProduct.total = await ProductCategory.countDocuments({
+   deleted: false
+})
+statistic.categoryProduct.active = await ProductCategory.countDocuments({
+   status: "active",
+   deleted: false
+})
+statistic.categoryProduct.inactive = await ProductCategory.countDocuments({
+   deleted: false,
+   status: "inactive"
+})
+
    res.render("admin/pages/dashboard/index.pug", {
-      pageTitle: "trang tổng quan"
+      pageTitle: "trang tổng quan",
+      statistic: statistic
    })
 }
