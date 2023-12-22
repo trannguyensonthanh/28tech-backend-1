9792724ec08dfbd5b1aa57b7b51854cc380c1237
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-22 15:50:53 +0700

    làm tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,13 +1,38 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model")
 //[get] /chat/
 module.exports.index = async (req, res) => {
-
-   // socket io
-   _io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
+  const userId = res.locals.user.id;
+  // socket io
+  _io.once("connection", (socket) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      // luu vao database
+      const chat = new Chat({
+        user_id: userId,
+        content: content,
+      });
+      await chat.save();
+    });
   });
-   // end socket io
 
+  // end socket io
+
+  // lấy ra data
+  const chats = await Chat.find({
+    deleted: false
+  });
+
+for (const chat of chats){
+  const infoUser = await User.findOne({
+    _id: chat.user_id
+  }).select("fullName");
+  chat.infoUser = infoUser;
+}
+
+
+  
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
-  })
-  }
+    chats : chats
+  });
+};
