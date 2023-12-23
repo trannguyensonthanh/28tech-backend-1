4b830222a2d3b84da25ff795e31ed9e5b387f873
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2023-12-23 16:13:37 +0700

    Typing tin nhắn

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -2,26 +2,42 @@ const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model")
 //[get] /chat/
 module.exports.index = async (req, res) => {
-  const userId = res.locals.user.id;
-  const fullName = res.locals.user.fullName
+  // let userId = res.locals.user.id;
+
+
   // socket io
   _io.once("connection", (socket) => {
     socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      const userSend = await User.findOne({
+        _id: content.userId
+      })
       // luu vao database
       const chat = new Chat({
-        user_id: userId,
-        content: content,
+        user_id: content.userId,
+        content: content.content,
       });
       await chat.save();
 // trả data về client
 
  _io.emit("SERVER_RETURN_MESSAGE", {
-  userId: userId,
-  fullName: fullName,
-  content: content
+  userId: content.userId,
+  fullName: userSend.fullName,
+  content: content.content
  })
 
     });
+
+    socket.on("CLIENT_SEND_TYPING", async (type) => {
+      const userTyping = await User.findOne({
+        _id: type.userIdTyping
+      })
+      console.log(type);
+        socket.broadcast.emit("SERVER_RETURN_TYPING", {
+        userId: type.userIdTyping,
+        fullName: userTyping.fullName,
+        type: type.show,
+       })
+    })
   });
 
   // end socket io
--- public/js/chat.js
+++ public/js/chat.js
@@ -8,8 +8,16 @@ if(formSendData){
     e.preventDefault();
     const content =  e.target.elements.content.value;
     if(content) {
-      socket.emit("CLIENT_SEND_MESSAGE", content);
+      const myId = document.querySelector("[my-id]").getAttribute("my-id");
+      socket.emit("CLIENT_SEND_MESSAGE", {
+        content: content,
+        userId: myId
+      });
       e.target.elements.content.value = "";
+      socket.emit("CLIENT_SEND_TYPING", {
+        show: "hidden",
+        userIdTyping: myId
+      });
     }
   })
 }
@@ -19,9 +27,8 @@ if(formSendData){
 // SERVER_RETURN_MESSAGE
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
 const myId = document.querySelector("[my-id]").getAttribute("my-id");
-console.log(data);
-console.log(myId);
   const body = document.querySelector(".chat .inner-body");
+  const boxTyping = document.querySelector(".inner-list-typing");
   const div = document.createElement("div");
   let htmlFullName = "";
   if(myId == data.userId) {
@@ -37,7 +44,7 @@ console.log(myId);
   ${htmlFullName}
   <div class="inner-content">${data.content}</div>
   `;
-  body.appendChild(div);
+  body.insertBefore(div, boxTyping);
   body.scrollTop = body.scrollHeight;
 })
 
@@ -48,10 +55,31 @@ const bodyChat= document.querySelector(".chat .inner-body");
 if (bodyChat){
   bodyChat.scrollTop = bodyChat.scrollHeight;
 }
-
 // end scroll chat to bottom
 
-// emoji-picker
+// SHOW TYPING
+var timeOut;
+const showTyping = () => {
+  const myId = document.querySelector("[my-id]").getAttribute("my-id");
+socket.emit("CLIENT_SEND_TYPING", {
+  show: "show",
+  userIdTyping: myId
+});
+  clearTimeout(timeOut);
+timeOut = setTimeout(() => {
+socket.emit("CLIENT_SEND_TYPING", {
+  show: "hidden",
+  userIdTyping: myId
+});
+}, 3000);
+}
+
+
+// END SHOW TYPING
+
+
+// EMOJI-PICKER
+
 // show popup
 const buttonIcon = document.querySelector(".button-icon");
 if (buttonIcon){
@@ -61,15 +89,68 @@ if (buttonIcon){
     tooltip.classList.toggle('shown')
   }
 }
-
 // insert icon to input
 const emojiPicker = document.querySelector("emoji-picker");
 if (emojiPicker){
   const inputChat = document.querySelector(".chat .inner-form input[name='content']");
+  
   emojiPicker.addEventListener("emoji-click", (event) => {
    const icon = event.detail.unicode;
    inputChat.value = inputChat.value + icon;
+   const end = inputChat.value.length
+   inputChat.setSelectionRange(end, end);
+   inputChat.focus();
+   // typing icon 
+   showTyping();
   })
 } 
 
-// end emoji-picker
+// end EMOJI-PICKER
+
+// SERVER_RETURN_TYPING
+const elementListTyping = document.querySelector(".chat .inner-list-typing");
+if (elementListTyping){
+  socket.on("SERVER_RETURN_TYPING", (data) => {
+    if (data.type == "show"){
+    const existTyping = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+   if (!existTyping) {
+    const bodyChat= document.querySelector(".chat .inner-body");
+     const boxTyping = document.createElement("div");
+    boxTyping.classList.add("box-typing");
+    boxTyping.setAttribute("user-id", data.userId);
+    boxTyping.innerHTML = 
+    `
+    <div class="inner-name">${data.fullName}</div>
+      <div class="inner-dots">
+        <span></span>
+        <span> </span>
+        <span> </span>
+      </div>
+    `  
+    elementListTyping.appendChild(boxTyping);
+    bodyChat.scrollTop = bodyChat.scrollHeight;
+   }
+    }
+    else{
+      const boxTypingRemove =  elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+      if (boxTypingRemove){
+        elementListTyping.removeChild(boxTypingRemove);
+      }
+    }
+  })
+}
+// end SERVER_RETURN_TYPING
+
+
+// CLIENT_SEND_TYPING
+const inputChatTyping = document.querySelector(".chat .inner-form input[name='content']");
+if (inputChatTyping){
+  inputChatTyping.addEventListener("keyup", () => {
+    const end = inputChatTyping.value.length
+    inputChatTyping.setSelectionRange(end, end);
+    inputChatTyping.focus();
+    showTyping();
+  })
+}
+
+// end CLIENT_SEND_TYPING
